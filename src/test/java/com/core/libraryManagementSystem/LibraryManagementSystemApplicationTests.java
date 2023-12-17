e5f3a2f259d613e90dcfca9722e02a6ffc5aa4ae
Ananta <anantashrestha45@gmail.com>
2023-12-17 16:47:54 +0545

    added something again

--- src/test/java/com/core/libraryManagementSystem/LibraryManagementSystemApplicationTests.java
+++ src/test/java/com/core/libraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.core.libraryManagementSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryManagementSystemApplicationTests.class)
 class LibraryManagementSystemApplicationTests {
 
 	@Test
